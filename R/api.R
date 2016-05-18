f5aa2cab8f2e254527b58b468a26000000559572
Kirill Sevastyanenko <kirillseva@gmail.com>
2016-05-18 10:48:59 -0500

    add more apis

--- R/api.R
+++ R/api.R
@@ -5,3 +5,19 @@ post_new_model <- function(payload) {
     content_type_json()
   ))
 }
+
+post_new_iteration <- function(payload) {
+  content(POST(
+    paste0(LOOP_URL(), NEW_ITERATION_PATH),
+    body = payload,
+    content_type_json()
+  ))
+}
+
+post_report_metric <- function(payload) {
+  content(POST(
+    paste0(LOOP_URL(), REPORT_METRIC_PATH),
+    body = payload,
+    content_type_json()
+  ))
+}
